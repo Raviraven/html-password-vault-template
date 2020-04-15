3b2f13ba3f1d960d3b47fd1ed6bbf7c6740714ca
Michał Kruszewski <kruszewski2@gmail.com>
2020-04-15 22:58:42 +0200

    js: hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,34 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
 // place your code below
 
+const navigation__button = document.querySelector(".navigation__button--js");
+navigation__button.addEventListener("click", (e) => {
+  const navigation__list = document.querySelector(".navigation__list--js");
+  navigation__list.classList.toggle("navigation__list--visible");
 
-console.log(`Hello world!`);
-
-
+  if (navigation__list.classList.contains("navigation__list--visible")) {
+    navigation__button.innerHTML = "X";
+  } else {
+    navigation__button.innerHTML = "☰";
+  }
+});
